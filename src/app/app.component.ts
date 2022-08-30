f69bc5ea7a9f1ab4a3647c6e17f217e003639d8f
Robert Cini <perejdbest@gmail.com>
2022-08-30 20:40:15 +0200

    -Initialised Angular -Created reactive form -Added validation for both username(email) and password input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularApp';
+  loginForm = new FormGroup({
+    username: new FormControl('',Validators.compose([Validators.required,Validators.email])),
+    password: new FormControl('',Validators.compose([Validators.required,Validators.minLength(6),Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]+$')])),
+  })
+
+  loginUser(){
+    console.warn(this.loginForm.value)
+  }
+
+  get username(){
+    return this.loginForm.get('username');
+  }
+
+  get password(){
+    return this.loginForm.get('password');
+  }
 }
