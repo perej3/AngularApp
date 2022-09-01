05b0731e508ae8482bf2a09a979c768bcf46aaf4
Robert Cini <perejdbest@gmail.com>
2022-09-01 10:28:18 +0200

    --Created offers component and added routing to it. --Introduced CSS Bootstrap library --Fixed UI (horiz/vert center alignments) for forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OffersComponent } from './offers/offers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'offers',
+    component: OffersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
